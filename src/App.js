73e88cf0f6249f4c76af7c532a342780c368cb13
uwaifo <mr.uwaifo@gmail.com>
2019-05-10 03:17:37 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { FormGroup, FormLabel, FormControl } from "react-bootstrap";
+import { initText } from "./preview";
+//import marked from 'marked'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let marked = require("marked");
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: initText
+    };
+  }
+
+  markdownHandler = markdown => {
+    this.setState({ markdown });
+  };
+  render() {
+    return (
+      <div className="App container">
+        <h4 className="display-4">Markdown Previewer</h4>
+        <div>
+          <FormGroup controlId="fg_id">
+            <FormLabel>Markdown Input</FormLabel>
+            <FormControl
+              as="textarea"
+              rows="6"
+              id="editor"
+              componentClass="textarea"
+              //placeholder={placeHolder}
+              value={this.state.markdown}
+              onChange={e => this.markdownHandler(e.target.value)}
+            />
+          </FormGroup>
+        </div>
+        <div>
+          <h1>Output</h1>
+          <div
+            id="preview"
+            dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/preview.js
@@ -0,0 +1,29 @@
+export const initText = `# Page Header
+## Title Sub-Heading
+  
+JSX Inline Code:  \`<div>{this.state.myComponent}</div>\`
+
+JavaScript Inline Code:
+\`\`\`
+ markdownHandler = markdown => {
+    this.setState({ markdown });
+  };
+\`\`\`
+
+
+1. Ordered list
+2. List Item
+3. List Item
+
+
+**Bold text.**
+_Italic text._
+**_Bold and italic text._**
+~~Striked out text.~~
+
+Link: [links](https://www.freecodecamp.com)
+> Block Quote
+
+
+![Logo](https://pbs.twimg.com/profile_images/687023870067347456/rRAcGKvC_400x400.png)
+`;
